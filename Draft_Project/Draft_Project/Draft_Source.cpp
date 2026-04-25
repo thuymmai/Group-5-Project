510cae72202d55244e8e2e55a73ea28565ebd2ad
thuymmai <thuymmai11@gmail.com>
2026-04-25 18:52:29 -0500

    Made some (unofficial) changes to Draft_Source.cpp, SearchEvent.h, SearchEventList.txt

--- Draft_Project/Draft_Project/Draft_Source.cpp
+++ Draft_Project/Draft_Project/Draft_Source.cpp
@@ -1,15 +1,44 @@
-//Ticket system
+//Ticket Hub
 //Thuy, Thi, Vasupradha
 
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <ctime>
+#include "Register.h"
 #include "LogIn.h"
+#include "SearchEvent.h"
 using namespace std;
 
 int main() {
 
+	//read from a text file
+	ifstream file("SearchEventList.txt");
 
+	vector<string> v;
+	string str;
+
+	while (file >> str) {
+		v.push_back(str);
+	}
+
+	copy(v.begin(), v.end(),
+		ostream_iterator<string>(cout, "\n"));
+
+	//generate random event
+	srand(time(0));
+	int randNum = rand() % 5 + 1;
+
+	
+	
+	switch (randNum) {
+	case 1: cout << "";
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+
+	}
 
 	return 0;
 }
--- Draft_Project/Draft_Project/SearchEvent.h
+++ Draft_Project/Draft_Project/SearchEvent.h
@@ -1,11 +1,37 @@
+//Thuy Mai-Woods
+
 #pragma once
 #ifndef SEARCH_EVENT_H
 #define SEARCH_EVENT_H
 
+#include <iostream>
+#include <iomanip>	//for setw later
 #include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+//Note: a vector with a struct for artist, location, and date of event
+//use a text file that is loaded into a vector
+//use getline
+
 class SearchEvent {
+
+public:
+	struct Event {
+		string artistName;
+		string venue;
+		string location;
+		string date;	//YYYY-MM-DD
+	};
+
+	SearchEvent();
+
+	//search events by artist name
+	vector<Event> artistName;
+
+
 
 };
 
